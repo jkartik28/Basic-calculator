6c0189f645f9d25f105c422dca101d95db8bd5bc
Kartik jain <kartikjain407@gmail.com>
2025-08-24 17:01:45 +0530

    Add files via upload

---
+++ Even_odd.py
@@ -0,0 +1,12 @@
+#  Check ODD or EVEN numbers
+
+print("Check the given number is ODD or EVEN...!")
+
+while True :
+
+    n = int(input("Enter a number : "))
+    
+    if n%2 == 0 :
+        print("The number is EVEN")
+    else :
+        print("The number is ODD")
